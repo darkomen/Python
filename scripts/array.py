ad45a27bd2299cfa2a80e632b0013b599a999c04
Santi <santiago.lopez.pina@gmail.com>
2015-12-11 11:16:42 +0100

    cambios menores

--- scripts/array.py
+++ scripts/array.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 # Scripts para generar un array con distintos números.
 # {270,271,272,272}
 
 strs="{"
-inicio=271
+inicio=290
 cuantos=20
-
+separacion=2
 for i in range(cuantos):
-    strs=strs+"{}".format(inicio+i)+","
+    strs=strs+"{}".format(inicio+(i*separacion))+","
 
 
-print(strs[:-1]+"}")
+print(strs[:-1]+"};")
