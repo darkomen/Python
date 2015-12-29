f002954ca9de119c9e785a3a24807d05ab0899f8
Santiago Lopez Pina <santiago.lopez.pina@gmail.com>
2015-12-29 11:31:43 +0100

    Script para guardar en un .txt una lista de proxies de una pagina web

---
+++ scripts/lista.proxys.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+with urllib.request.urlopen("http://www.sslproxies.org/") as url:
+    s = url.read()
+bs = BeautifulSoup(s)
+table = bs.find('table',{'class':'display fpltable' ,'id':'proxylisttable'})
+rows = table.find_all('tr')
+results=[]
+for row in rows:
+	table_data = row.find_all('td')
+	if table_data:
+		results.append([data.get_text() for data in table_data])
+
+with open("proxies.txt",'w') as f:
+	for ip in results:
+		f.write("{}:{}\n".format(ip[0],ip[1]))
