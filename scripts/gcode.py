ad8782af69cca12d386d017c899cfc944c141701
Santi <santiago.lopez.pina@gmail.com>
2016-01-07 10:40:43 +0100

    Añadidos comentarios al script

--- scripts/gcode.py
+++ scripts/gcode.py
@@ -1,20 +1,21 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Script para modificar la temperatura de la cama caliente en un gcode
-# Autor: Santiago López Pina
+# Autor: Santiago LÃ³pez Pina
 # Fecha: 28 de Octubre de 2015
 
 # Paquetes para parsear los argumentos en la llamada
 import argparse
-import json
+
 
 # Variable con los argumentos
 ap = argparse.ArgumentParser()
 # Argumento con el nombre del fichero gcode a editar
 ap.add_argument("-g", "--gcode", required=True,
-	help="path to the Gcode configuration file")
+                help="path to the Gcode configuration file")
 # Argumento con el setpoint de temperatura de la cama caliente
 ap.add_argument("-t", "--temp", required=True,
-	help="Temp to the set the bed")
+                help="Temp to the set the bed")
 # Diccionario con los argumentos anteriores
 args = vars(ap.parse_args())
 # Variable con el nombre del fichero a editar
@@ -31,7 +32,7 @@ f1 = open(f_gcode, 'r')
 f2 = open(f_gcode2, 'w')
 # Por cada linea en el fichero de origen
 for line in f1:
-	# Escribimos en el fichero de destino y reemplazamos la linea con el comando de la cama caliente
+    # Escribimos en el fichero de destino y reemplazamos la linea con el comando de la cama caliente
     f2.write(line.replace(';M190 S70 ;Uncomment to add your own bed temperature line', command))
 # Cerramos ambos ficheros
 f1.close()
