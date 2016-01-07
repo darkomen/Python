df6d20ef750177486815f2de0da17c5a806ce9cd
Santi <santiago.lopez.pina@gmail.com>
2016-01-07 10:41:20 +0100

    Script para descargar un fichero de una página web

---
+++ scripts/download.py
@@ -0,0 +1,16 @@
+import urllib.request
+
+def main():
+    for i in range(10):
+        str="https://static.lwn.net/images/pdf/LDD3/ch18.pdf"
+        download_file("https://static.lwn.net/images/pdf/LDD3/ch18.pdf")
+
+def download_file(download_url):
+    response = urllib.request.urlopen(download_url)
+    file = open("document.pdf", 'wb')
+    file.write(response.read() )
+    file.close()
+    print("Completed")
+
+if __name__ == "__main__":
+    main()
