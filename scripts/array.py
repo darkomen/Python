41a575075dbb69c527add9005ae4b7a926c043d0
Santi <santiago.lopez.pina@gmail.com>
2016-01-07 10:40:29 +0100

    Generar un array con distitnos valores numéricos

--- scripts/array.py
+++ scripts/array.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Scripts para generar un array con distintos números.
+# Scripts para generar un array con distintos nï¿½meros.
 # {270,271,272,272}
 
-strs="{"
-inicio=290
-cuantos=20
-separacion=2
-for i in range(cuantos):
-    strs=strs+"{}".format(inicio+(i*separacion))+","
+strs = "{"
+inicio = 260
+cuantos = 20
+separacion = 1
 
+for i in range(cuantos):
+    strs = strs+"{}".format(inicio+(i*separacion))+","
 
 print(strs[:-1]+"};")
