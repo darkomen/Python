ec58d7959409c513847dee88493a0b1b901767fd
Santiago Lopez Pina <santiago.lopez.pina@gmail.com>
2016-01-31 11:49:45 +0100

    modificada regla para obtener las pizzas y los menus

--- scripts/carta.py
+++ scripts/carta.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
+
+url =  'http://pizzeriascarlos.com/nuestracarta.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.content,'lxml')
+links = soup.select('div a[href^=indexpizza_files]')
+for link in links:
+	name = [link['title'] for link in links]
+	ingredients = [link.text[len(name[i])+2:] for i,link in enumerate(links)]
+
+pizzas = dict(zip(name,ingredients))
+print(pizzas['Granjera'])
 
 """
 with urllib.request.urlopen("http://pizzeriascarlos.com/nuestracarta.html") as url:
@@ -31,7 +42,7 @@ for i, elements in enumerate(links):
 
 """
 # Vadepizza
-
+"""
 from urllib.request import Request, urlopen
 
 req = Request('https://www.laneveraroja.com/restaurant/n9mu/vadepizza-rivas-centro', headers={'User-Agent': 'Mozilla/5.0'})
@@ -53,7 +64,7 @@ ofertas = [elements.get('title') for elements in links ]
 precios =[elements.get_text().replace('€','').replace('\n','').replace(' ','') for elements in precio]
 kk = dict(zip(ofertas,precios))
 print(kk)
-"""
+
 for i in range(len(precios)):
 	print("{}: {}".format(ofertas[i],precios[i][1].replace(' ','')))
 """
