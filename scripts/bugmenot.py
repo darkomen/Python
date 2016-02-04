1a9a1c4b0a5fbfc8ad4d28b076154ca8628be758
Santi <santiago.lopez.pina@gmail.com>
2016-02-04 15:32:35 +0100

    script para adquirir usuario/contraseña de de una web mediante bugmenot

---
+++ scripts/bugmenot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Script para descargar todas los nombres de los superheroes marvel
+# y elegir de forma aleatoria 4
+
+import requests
+import random
+from bs4 import BeautifulSoup
+
+url =  'http://bugmenot.com/view/123dapp.com'
+
+response = requests.get(url)
+print(response)
+soup = BeautifulSoup(response.content, 'html.parser')
+names = soup.select('article.account dl kbd')
+
+print((names.text))
