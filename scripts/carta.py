722550c1f9fe0fcafab0d437c5f7965847d3c458
Santi <santiago.lopez.pina@gmail.com>
2016-02-10 08:57:24 +0100

    Modificado fichero para cumplir con PEP8

--- scripts/carta.py
+++ scripts/carta.py
@@ -4,26 +4,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url =  'http://pizzeriascarlos.com/nuestracarta.html'
+url = 'http://pizzeriascarlos.com/nuestracarta.html'
 response = requests.get(url)
-soup = BeautifulSoup(response.content,'lxml')
+soup = BeautifulSoup(response.content, 'lxml')
 links = soup.select('div a[href^=indexpizza_files]')
 for link in links:
-	name = [link['title'] for link in links]
-	ingredients = [link.text[len(name[i])+2:] for i,link in enumerate(links)]
+    name = [link['title'] for link in links]
+    ingredients = [link.text[len(name[i])+2:] for i, link in enumerate(links)]
 
-pizzas = dict(zip(name,ingredients))
+pizzas = dict(zip(name, ingredients))
 print(pizzas['Granjera'])
 
 """
-with urllib.request.urlopen("http://pizzeriascarlos.com/nuestracarta.html") as url:
+with urllib.request.urlopen(
+    "http://pizzeriascarlos.com/nuestracarta.html") as url:
     s = url.read()
 bs = BeautifulSoup(s,'lxml')
 
 primary = bs.find_all('div', class_='content group three')
 links = primary[0].find_all('a',class_='vlightbox1')
 for i, elements in enumerate(links):
-	print((elements.get('title')))	
+    print((elements.get('title')))
 
 """
 """
@@ -32,12 +33,13 @@ with urllib.request.urlopen("http://www.telepizza.es/productos/pizzas") as url:
     s = url.read()
 bs = BeautifulSoup(s,'lxml')
 
-primary = bs.find_all('div', class_='content_with_highlighted_sidebar wrapper row')
+primary = bs.find_all('div',
+                      class_='content_with_highlighted_sidebar wrapper row')
 #mod_product_list
 #content_with_highlighted_sidebar wrapper row
 links = primary[0].find_all('img',class_='productImage')
 for i, elements in enumerate(links):
-	print((elements.get('title')))	
+    print((elements.get('title')))
 
 
 """
@@ -56,15 +58,15 @@ primary = bs.find_all('article', class_='menu__category')
 links = primary[0].find_all('div',class_='menu-item__title')
 precio = primary[0].find_all('div',class_='menu-item__variation__price ')
 #for i, elements in enumerate(links), enumerate(precio):
-#	print((elements.get('title')))	
+#	print((elements.get('title')))
 #links = primary[0].find_all('div',class_='menu-item__variation__price ')
 #for i, elements in enumerate(links):
-#	print((elements.get_text()))	
+#	print((elements.get_text()))
 ofertas = [elements.get('title') for elements in links ]
 precios =[elements.get_text().replace('€','').replace('\n','').replace(' ','') for elements in precio]
 kk = dict(zip(ofertas,precios))
 print(kk)
 
 for i in range(len(precios)):
-	print("{}: {}".format(ofertas[i],precios[i][1].replace(' ','')))
+    print("{}: {}".format(ofertas[i],precios[i][1].replace(' ','')))
 """
