d3c59b6ef011e15539186f9b9501b3ba944eed42
Santiago Lopez Pina <santiago.lopez.pina@gmail.com>
2016-03-06 12:56:11 +0100

    Comprobación si la tira cómica existe para no descargarla

--- scripts/xkcd_en.py
+++ scripts/xkcd_en.py
@@ -3,29 +3,69 @@
 
 import requests
 import random
+import os
+import time
 from bs4 import BeautifulSoup
 
-url = 'http://xkcd.com/archive/'
-home = 'http://xkcd.com'
+class strip():
+    def __init__(self):
+        self.__count = 0
+        self.__url = 'http://xkcd.com/archive/'
+        self.__home = 'http://xkcd.com'
+        self.__folder = './xkcd/'
+        self.__start_time = 0
+        self.__end_time = 0
+        self.__elapsed_time = 0
+        if not os.path.exists(self.__folder):
+            os.makedirs(self.__folder)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'lxml')
-link = soup.find('div', id='middleContainer')
-names = link.find_all('a')
+    def get_soup(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            return soup
+        else:
+            return False
 
-for name in names:
-    strip_url = (home+name['href'])
-    response = requests.get(strip_url)
-    soup = BeautifulSoup(response.content, 'lxml')
-    div_img = soup.find('div', id='comic')
-    img = div_img.find('img')
-    try:
-        link_img = "http:" + img['src']
-        file_img = './xkcd-en/' + str(img['alt']).replace(' ','') + '.png'
-        response = requests.get(link_img)
-        file = open(str(file_img), 'wb')
-        file.write(response.content)
-        file.close()
-    except:
-        pass
+    def check_file(self, file):
+        if os.path.exists(file):
+            return True
+        else:
+            return False
 
+    def main(self):
+        self.__start_time = time.time()
+        soup = self.get_soup(self.__url)
+        link = soup.find('div', id='middleContainer')
+        names = link.find_all('a')
+
+        for name in names:
+            strip_url = self.__home + name['href']
+            soup = self.get_soup(strip_url)
+            div_img = soup.find('div', id='comic')
+            img = div_img.find('img')
+
+            try:
+                link_img = "http:" + img['src']
+                file_img = self.__folder + str(img['alt']).replace(' ','') + '.png'
+                if self.check_file(file_img):
+                    print("fichero existe")
+                    pass
+                else:
+                    response = requests.get(link_img)
+                    file = open(str(file_img), 'wb')
+                    file.write(response.content)
+                    file.close()
+                    print("Tira cómica salvada en: {}".format(file_img))
+                    self.__count = self.__count + 1
+            except:
+                print("Se ha producido un error al acceder a la tira")
+                pass
+
+        __end_time = time.time()
+        __elapsed_time = __end_time - __start_time
+        print("Descargadas un total de {} tiras de {} en {} s".format(self.__count, self.__home, self.__end_time))
+
+if __name__ == '__main__':
+    xkcd = strip()
+    xkcd.main()
